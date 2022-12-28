b0e08badb3e5bc13adbeee8d12696f814521b79c
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2022-12-28 22:29:22 +0700

    fix: only get post and story from following people

--- lib/services/story_service.dart
+++ lib/services/story_service.dart
@@ -1,10 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:it008_social_media/models/story_model.dart';
+import 'package:it008_social_media/services/user_service.dart';
 import 'package:it008_social_media/utils/firebase_consts.dart';
 
 class StoryService {
   static Future<List<List<Story>>> getStoriesFromOtherPeople() async {
     List<List<Story>> temp = [];
+
+    List followingPeople = await UserService.getFollowingPeople(user!.uid);
+
     // get stories from following people
     QuerySnapshot<Object?> snapshot =
         await storiesRef.get(); // all documentSnapshot with user name
@@ -14,10 +18,11 @@ class StoryService {
           .doc(doc.id)
           .collection('stories')
           .where('whoCanSee', arrayContains: user!.uid)
-          .limit(20)
           .where('createdTime',
               isGreaterThan: Timestamp.fromDate(
                   DateTime.now().subtract(const Duration(days: 1))))
+          .where('userId', whereIn: followingPeople)
+          .limit(20)
           .get();
       final newStory = storiesSnapshot.docs
           .map((doc) => Story.fromJson(doc.data() as Map<String, dynamic>))
